edc004895a9e31c0345ca469966dceff59e85143
Noah Gleason <noah.gleason1@gmail.com>
2018-02-09 22:31:55 -0500

    Add install script

---
+++ installDependencies.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not run as root, exiting."
+    exit
+fi
+export
+mkdir installDependencies
+cd installDependencies
+wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
+tar -xf protobuf-2.6.1.tar.gz
+cd protobuf-2.6.1
+./configure
+make -j$(nproc)
+make install
+ldconfig
+cd ..
+wget https://github.com/zeromq/libzmq/releases/download/v4.2.2/zeromq-4.2.2.tar.gz
+tar -xf zeromq-4.2.2.tar.gz
+cd zeromq-4.2.2
+./configure
+make -j$(nproc)
+make install
+cd ..
+wget https://github.com/zeromq/cppzmq/archive/v4.2.2.tar.gz
+tar -xf v4.2.2.tar.gz
+cd cppzmq-4.2.2/
+mkdir build
+cd build
+cmake ..
+make -j$(nproc) install
+cd ../../
+rm -rf installDependencies
